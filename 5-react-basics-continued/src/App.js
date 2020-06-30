a870dcec608e07d3a72b85ff4ce0191018f92a65
Matthijs Boet <m.boet@chello.nl>
2020-07-01 00:33:29 +0200

    halfway through React basics continued

---
+++ 5-react-basics-continued/src/App.js
@@ -0,0 +1,42 @@
+// state, this.setState = hold/access/change data of components
+// props = read only
+
+// named and default import/export
+// only one default export per file
+import React, {Component} from 'react';
+// imported selected variables and object
+import {name, age, person} from './data';
+// default import can be named anything
+import Banner from './components/Header/Banner'
+// imported wildcard must use data.name data.job
+import * as data from './data'
+
+// functional component use when state is NOT needed
+
+// const App = () => (
+//   <section>
+//     <Banner/>
+//     <p>{name}</p>
+//     <p>{age}</p>
+//     <p>{person.name}</p>
+//     <p>{person.age}</p>
+//   </section>
+// )
+
+// class based component use when state is needed
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <Banner/>
+        <p>{name}</p>
+        <p>{age}</p>
+        <p>{person.name}</p>
+        <p>{person.age}</p>
+      </section>
+    );
+  }
+}
+
+export default App;
---
+++ 5-react-basics-continued/src/components/Header/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {name} from '../../data'
+
+// default export
+export default function Banner() {
+  return (
+    <div>
+      <h1>Hello from banner {name}</h1>
+    </div>
+  )
+}
