7ed415c256d2c9aabffe18f93a0d679a1652c1cf
Matthijs Boet <m.boet@chello.nl>
2020-07-06 18:54:09 +0200

    changed TodoItem class to specific classname of icon

--- 8-todo-list-project/src/components/TodoItem.js
+++ 8-todo-list-project/src/components/TodoItem.js
@@ -9,10 +9,10 @@ export default class TodoItem extends Component {
         <h6>{title}</h6>
         <div className="todo-icon">
           <span className="mx-2 text-success" onClick={handleEdit}>
-            <i className="fas fa-pen" />
+            <i className="fas fa-pen icon" />
           </span>
           <span className="mx-2 text-danger" onClick={handleDelete}>
-            <i className="fas fa-trash" />
+            <i className="fas fa-trash icon" />
           </span>
         </div>
       </li>
