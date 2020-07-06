090bc04724eb6770f212dbe85768c5e7e7aeb95e
Matthijs Boet <m.boet@chello.nl>
2020-07-06 19:07:56 +0200

    added inline styles

--- 8-todo-list-project/src/components/TodoItem.js
+++ 8-todo-list-project/src/components/TodoItem.js
@@ -8,10 +8,18 @@ export default class TodoItem extends Component {
       <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
         <h6>{title}</h6>
         <div className="todo-icon">
-          <span className="mx-2 text-success" onClick={handleEdit}>
+          <span
+            className="mx-2 text-success"
+            onClick={handleEdit}
+            style={{ cursor: "pointer" }}
+          >
             <i className="fas fa-pen icon" />
           </span>
-          <span className="mx-2 text-danger" onClick={handleDelete}>
+          <span
+            className="mx-2 text-danger"
+            onClick={handleDelete}
+            style={{ cursor: "pointer" }}
+          >
             <i className="fas fa-trash icon" />
           </span>
         </div>
