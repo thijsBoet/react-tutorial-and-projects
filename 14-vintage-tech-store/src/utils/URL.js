9ed399d38ebee6d9107228e7103c85b521c3d0b4
Matthijs Boet <m.boet@chello.nl>
2020-10-13 22:45:39 +0200

    setting up STRAPI API

--- 14-vintage-tech-store/src/utils/URL.js
+++ 14-vintage-tech-store/src/utils/URL.js
@@ -1,3 +1,2 @@
 // url
-export default "http://johnsmilgatutorials.com/projects/react-tech-store-v2";
-
+export default "http://localhost:1337";
---
+++ store-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4f76f9f8-a070-4e2e-a36d-4019c29267a1'
+};
